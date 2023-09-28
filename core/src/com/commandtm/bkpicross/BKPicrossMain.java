51b791ba1ccaca12200a920fb84129269a49374c
colin-every <commandnoobmc23@gmail.com>
2023-09-28 10:18:06 -0400

    Number Textures

--- core/src/com/commandtm/bkpicross/BKPicrossMain.java
+++ core/src/com/commandtm/bkpicross/BKPicrossMain.java
@@ -12,6 +12,16 @@ public class BKPicrossMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture fill;
 	Texture empty;
+	Texture nine;
+	Texture eight;
+	Texture seven;
+	Texture six;
+	Texture five;
+	Texture four;
+	Texture three;
+	Texture two;
+	Texture one;
+	Texture zero;
 	Vector2 touchPos;
 	int clickY;
 	int clickX;
@@ -24,6 +34,16 @@ public class BKPicrossMain extends ApplicationAdapter {
 		touchPos = new Vector2();
 		fill  = new Texture("filled.png");
 		empty = new Texture("empty.png");
+		zero = new Texture("Numbers/0.png");
+		one = new Texture("Numbers/1.png");
+		two = new Texture("Numbers/2.png");
+		three = new Texture("Numbers/3.png");
+		four = new Texture("Numbers/4.png");
+		five = new Texture("Numbers/5.png");
+		six = new Texture("Numbers/6.png");
+		seven = new Texture("Numbers/7.png");
+		eight = new Texture("Numbers/8.png");
+		nine = new Texture("Numbers/9.png");
 	}
 
 	@Override
@@ -35,6 +55,18 @@ public class BKPicrossMain extends ApplicationAdapter {
 				batch.draw((Board.board[k][i]) ? fill : empty, ((32*i)+160), (480-(32*k)));
 			}
 		}
+		batch.draw(zero, 128, 480);
+		batch.draw(one, 112, 480);
+		batch.draw(nine, 128, 448);
+		batch.draw(eight, 128, 416);
+		batch.draw(seven, 128, 384);
+		batch.draw(six, 128, 352);
+		batch.draw(five, 128, 320);
+		batch.draw(four, 128, 288);
+		batch.draw(three, 128, 256);
+		batch.draw(two, 128, 224);
+		batch.draw(one, 128, 192);
+		batch.draw(zero, 128, 160);
 		batch.end();
 		if (Gdx.input.isTouched()){
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY());
