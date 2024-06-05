eb1bd43e344589e122831c0ae6f9c2708bb4309c
CommandTM <commandnoobmc23@gmail.com>
2024-06-05 08:08:27 -0400

    Resizable Text Client

--- Scripts/TextClient.cs
+++ Scripts/TextClient.cs
@@ -18,11 +18,15 @@ public partial class TextClient : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("Send Message"))
+		if (Input.IsActionJustPressed("Send Message") && !_typeLine.Text.Equals(""))
 		{
 			Universal.Session.Say(_typeLine.Text);
 			_typeLine.Text = "";
 		}
+
+		_typeLine.Size = new Vector2(GetParent<Window>().Size.X, _typeLine.Size.Y);
+		_display.Size = new Vector2(GetParent<Window>().Size.X, GetParent<Window>().Size.Y - 31);
+		_typeLine.Position = new Vector2(_typeLine.Position.X, _display.Size.Y);
 	}
 
 	public void PushMessage(LogMessage message)
